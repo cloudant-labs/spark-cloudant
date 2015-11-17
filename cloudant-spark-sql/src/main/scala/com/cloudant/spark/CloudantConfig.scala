c65e9c50169c22897b08b0bdfbbb50fe9dc53d1b
Mayya Sharipova <mayyas@ca.ibm.com>
2015-11-17 08:27:17 -0500

    Raise an exception during save() errors
    Raise an exception when data from Spark are saved
    to a db that doesn't exist in Cloudant
    
    Syntax formatting
    
    BugzID: 56197

--- cloudant-spark-sql/src/main/scala/com/cloudant/spark/CloudantConfig.scala
+++ cloudant-spark-sql/src/main/scala/com/cloudant/spark/CloudantConfig.scala
@@ -29,95 +29,111 @@ import play.api.libs.json.JsNumber
 /**
  * @author yanglei
  */
-@serializable case class CloudantConfig(val host: String, val dbName: String, val indexName: String = null)(implicit val username: String, val password: String, val partitions:Int, val maxInPartition: Int, val minInPartition:Int, val requestTimeout:Long,val concurrentSave:Int, val bulkSize: Int) extends JsonStoreConfig{
+@serializable case class CloudantConfig(val host: String, val dbName: String,
+    val indexName: String = null)
+    (implicit val username: String, val password: String,
+     val partitions:Int, val maxInPartition: Int, val minInPartition:Int,
+     val requestTimeout:Long,val concurrentSave:Int, val bulkSize: Int)
+    extends JsonStoreConfig{
   
-    private lazy val dbUrl = {"https://"+ host+"/"+dbName}
+  private lazy val dbUrl = {"https://"+ host+"/"+dbName}
 
-    val pkField = "_id"
-    val defaultIndex = "_all_docs" // "_changes" does not work for partition
+  val pkField = "_id"
+  val defaultIndex = "_all_docs" // "_changes" does not work for partition
 
-    override def getPostUrl(): String ={dbUrl}
-    override def getLastUrl(skip: Int): String = {
-      if (skip ==0 ) null
-      else s"$dbUrl/$defaultIndex?limit=$skip"
-    }
-    override def getLastNum(result: JsValue): JsValue = {result \ "last_seq"}
-    override def getTotalUrl(url: String) = {
-      if (url.contains('?')) url+"&limit=1"
-      else  url+"?limit=1"
-    }
+  override def getPostUrl(): String ={dbUrl}
+  
+  override def getLastUrl(skip: Int): String = {
+    if (skip ==0 ) null
+    else s"$dbUrl/$defaultIndex?limit=$skip"
+  }
+  
+  override def getLastNum(result: JsValue): JsValue = {result \ "last_seq"}
+  
+  override def getTotalUrl(url: String) = {
+    if (url.contains('?')) url+"&limit=1"
+    else  url+"?limit=1"
+  }
 
-    override def allowPartition(): Boolean = {indexName==null}
-    def getOneUrl(): String = { dbUrl+ "/_all_docs?limit=1&include_docs=true"}
+  override def getDbname(): String ={
+    dbName
+  }
+
+  override def allowPartition(): Boolean = {indexName==null}
     
-    def getRangeUrl(field: String = null, start: Any = null, startInclusive:Boolean = false, end:Any =null, endInclusive: Boolean =false,includeDoc: Boolean = true): (String, Boolean) = {
-          val (url:String, pusheddown:Boolean) = calculate(field, start, startInclusive, end, endInclusive)
-          if (includeDoc)
-          {
-            if (url.indexOf('?')>0) (url+"&include_docs=true",pusheddown)
-            else (url+"?include_docs=true",pusheddown)
-          }else
-           ( url, pusheddown)
-    }
+  def getOneUrl(): String = { dbUrl+ "/_all_docs?limit=1&include_docs=true"}
     
-    private def calculate(field: String, start: Any, startInclusive: Boolean, end:Any, endInclusive: Boolean): (String, Boolean) = {
-      if (field!=null && field.equals(pkField))
-      {
-          var condition = ""
-          if (start!=null && end!=null && start.equals(end))
-          {
-             condition += "?key=" + start 
-          }else
-          {
-            if (start != null)
-                condition += "?startkey=" + start 
-            if (end !=null)
-            {
-                if (start !=null)
-                  condition += "&"
-                else
-                  condition += "?"
-                condition += "endkey=" + end
-            }
+  def getRangeUrl(field: String = null, start: Any = null, 
+      startInclusive:Boolean = false, end:Any =null, 
+      endInclusive: Boolean =false, 
+      includeDoc: Boolean = true): (String, Boolean) = {
+    val (url:String, pusheddown:Boolean) = calculate(field, start, 
+      startInclusive, end, endInclusive)
+    if (includeDoc){
+      if (url.indexOf('?')>0) (url+"&include_docs=true",pusheddown)
+      else (url+"?include_docs=true",pusheddown)
+    }else
+       ( url, pusheddown)
+  }
+    
+  private def calculate(field: String, start: Any, startInclusive: Boolean, 
+      end:Any, endInclusive: Boolean): (String, Boolean) = {
+    if (field!=null && field.equals(pkField)){
+      var condition = ""
+      if (start!=null && end!=null && start.equals(end)){
+        condition += "?key=" + start 
+      }else{
+        if (start != null)
+            condition += "?startkey=" + start 
+        if (end !=null){
+          if (start !=null)
+            condition += "&"
+          else
+            condition += "?"
+            condition += "endkey=" + end
           }
-           (dbUrl + "/_all_docs"+URLEncoder.encode(condition,"UTF-8"), true)
-      }else if (indexName!=null) //  push down to indexName
-      {
-          val condition = calculateCondition(field, start, startInclusive, end, endInclusive)
-          (dbUrl+"/"+indexName+"?q="+condition, true)
+      }
+      (dbUrl + "/_all_docs"+URLEncoder.encode(condition,"UTF-8"), true)
+    }else if (indexName!=null){ //  push down to indexName
+        val condition = calculateCondition(field, start, startInclusive, 
+            end, endInclusive)
+        (dbUrl+"/"+indexName+"?q="+condition, true)
       }else
         (s"$dbUrl/$defaultIndex" ,false)
-    }
+  }
 
-    def getSubSetUrl (url: String, skip: Int, limit: Int)(implicit convertSkip:(Int) => String): String ={
-      val suffix = {
-        if (url.indexOf("_all_docs")>0) "include_docs=true&limit="+limit+"&skip="+skip
-        else if (url.indexOf("_changes")>0) "include_docs=true&limit="+limit+"&since="+convertSkip(skip)
-        else "include_docs=true&limit="+limit // TODO Index query does not support subset query. Should disable Partitioned loading?
-      }
-      if (url.indexOf('?')>0) url+"&"+suffix
-      else url+"?"+suffix
+  def getSubSetUrl (url: String, skip: Int, limit: Int)
+      (implicit convertSkip:(Int) => String): String ={
+    val suffix = {
+      if (url.indexOf("_all_docs")>0) "include_docs=true&limit=" + 
+        limit + "&skip=" + skip
+      else if (url.indexOf("_changes")>0) "include_docs=true&limit=" + 
+          limit + "&since=" + convertSkip(skip)
+      else "include_docs=true&limit=" + limit // TODO Index query does not support subset query. Should disable Partitioned loading?
     }
+    if (url.indexOf('?')>0) url+"&"+suffix
+    else url+"?"+suffix
+  }
     
-    def getTotalRows(result: JsValue): Int = {
-        val value = result \ "total_rows"
-        value match {
-            case s : JsUndefined => (result \ "pending").as[JsNumber].value.intValue() +1
-            case _ =>  value.as[JsNumber].value.intValue()
-          }
+  def getTotalRows(result: JsValue): Int = {
+    val value = result \ "total_rows"
+    value match {
+      case s : JsUndefined => 
+        (result \ "pending").as[JsNumber].value.intValue() + 1
+      case _ =>  value.as[JsNumber].value.intValue()
     }
+  }
     
-    def getRows(result: JsValue): Seq[JsValue] = {
-        result \\ "doc"
-    }
+  def getRows(result: JsValue): Seq[JsValue] = {
+    result \\ "doc"
+  }
     
-    override def getBulkPostUrl(): String = {
-      dbUrl + "/_bulk_docs"
-    }
+  override def getBulkPostUrl(): String = {
+    dbUrl + "/_bulk_docs"
+  }
     
-    override def getBulkRows(rows: Array[String]): String = {
-      val docs = rows.map { x => Json.parse(x) }
-      Json.stringify(Json.obj("docs" -> Json.toJson(docs.toSeq)))
-    }
-
+  override def getBulkRows(rows: Array[String]): String = {
+    val docs = rows.map { x => Json.parse(x) }
+    Json.stringify(Json.obj("docs" -> Json.toJson(docs.toSeq)))
+  }
 }
--- cloudant-spark-sql/src/main/scala/com/cloudant/spark/common/JsonStoreDataAccess.scala
+++ cloudant-spark-sql/src/main/scala/com/cloudant/spark/common/JsonStoreDataAccess.scala
@@ -20,23 +20,16 @@ import spray.client.pipelining._
 import scala.concurrent._
 import akka.actor.ActorSystem
 import akka.event.Logging
-import scala.util.{Success, Failure}
-import akka.actor.Actor
 import akka.util.Timeout
 import play.api.libs.json.Json
-import play.api.libs.json.JsArray
 import play.api.libs.json.JsValue
 import scala.collection.mutable.HashMap
 import play.api.libs.json.JsNull
 import org.apache.spark.sql.sources._
 import org.apache.spark.SparkEnv
-import org.apache.spark.sql.Row
-import org.apache.spark.rdd.RDD
 import spray.httpx.marshalling.Marshaller
-import spray.http.StatusCodes.Success
 import play.api.libs.json.JsString
 import com.typesafe.config.ConfigFactory
-import java.util.UUID
 import scala.util.Random
 
 
@@ -44,170 +37,177 @@ import scala.util.Random
  * @author yanglei
  */
 class JsonStoreDataAccess (config: JsonStoreConfig)  {
+  implicit lazy val timeout = {Timeout(config.requestTimeout)}
+  lazy val envSystem = {SparkEnv.get.actorSystem}
+  lazy val logger = {Logging(envSystem, getClass)}
 
-      implicit lazy val timeout = {Timeout(config.requestTimeout)}
-      lazy val envSystem = {SparkEnv.get.actorSystem}
+  private lazy val validCredentials: BasicHttpCredentials = {
+    if (config.username !=null) BasicHttpCredentials(config.username, 
+      config.password)
+    else null
+  }
 
-      lazy val logger = {Logging(envSystem, getClass)}
+  def getOne()( implicit columns: Array[String] = null) = {
+    this.getQueryResult[Seq[String]](config.getOneUrl, processAll)
+  }
+
+  def getAll[T](url: String)
+      (implicit columns: Array[String] = null,
+      attrToFilters: Map[String, Array[Filter]] =null) = {
+    this.getQueryResult[Seq[String]](url,processAll)
+  }
+
+  def getIterator(skip: Int, limit: Int, url: String)
+      (implicit columns: Array[String] = null,
+      attrToFilters: Map[String, Array[Filter]] = null) = {
+    implicit def convertSkip(skip: Int): String  = {
+      val url = config.getLastUrl(skip)
+      if (url == null) 
+        skip.toString()
+      else  
+        this.getQueryResult[String](url,
+          {result => config.getLastNum(Json.parse(result)).as[JsString].value})
+    }
+    val newUrl = config.getSubSetUrl(url,skip, limit)
+    this.getQueryResult[Iterator[String]](newUrl, processIterator)
+  }
+
+  def getTotalRows(url: String) : Int ={
+    val totalUrl = config.getTotalUrl(url)
+    this.getQueryResult[Int](totalUrl,
+        {result => config.getTotalRows(Json.parse(result))})
+  }
+
+  private def processAll (result: String)
+      (implicit columns: Array[String], 
+      attrToFilters: Map[String, Array[Filter]] = null)= {
+    logger.debug(s"processAll columns:$columns, attrToFilters:$attrToFilters")
+    val jsonResult = Json.parse(result)
+    var rows = config.getRows(jsonResult )
+    rows.map(r =>  convert(r))
+  }
       
-      private lazy val validCredentials: BasicHttpCredentials = {
-            if (config.username !=null) BasicHttpCredentials(config.username, config.password)
-            else null
-      }
+  private def processIterator (result: String)
+    (implicit columns: Array[String], 
+    attrToFilters: Map[String, Array[Filter]] =null)= {
+    processAll(result).iterator
+  }
 
+  private def convert(rec:JsValue)(implicit columns: Array[String]): String = {
+    if (columns == null) return Json.stringify(Json.toJson(rec))
+    val m = new HashMap[String, JsValue]()
+    for ( x <-columns){
+        val field = JsonUtil.getField(rec, x).getOrElse(JsNull)
+        m.put(x,  field)
+    }
+    val result = Json.stringify(Json.toJson(m.toMap))
+    logger.debug(s"converted: $result")
+    result
+  }
 
-      def getOne()( implicit columns: Array[String] = null) = {
-          this.getQueryResult[Seq[String]](config.getOneUrl,processAll)
-      }
-      
-      def getAll[T](url: String )(implicit columns: Array[String] =null,attrToFilters: Map[String, Array[Filter]] =null) ={
-          this.getQueryResult[Seq[String]](url,processAll)
-      }
+  private def getSystem(): (ActorSystem, Boolean) = {
+    val checkSprayConfig = envSystem.settings.config.withFallback(ConfigFactory.parseString("""
+        spray = NOT_FOUND
+     """) )
+    val sprayValue = checkSprayConfig.getValue("spray").unwrapped().toString()
+    if ( !sprayValue.equals("NOT_FOUND")) // The env actorSystem loaded spary config
+    {
+      logger.info("reuse SparkEnv ActorSystem as it contains spray")
+      (envSystem, true)
+    }
+    else{
+      logger.info("create new ActorSystem as the SparkEnv one does not contain spray")
+      val classLoader = this.getClass.getClassLoader
+      val myconfig = ConfigFactory.load(classLoader)// force config from my classloader
+      val nextRamdomNum = new Random().nextInt
+        (ActorSystem("CloudantSpark-"+nextRamdomNum,myconfig,classLoader), false)
+    }
+  }
+  
+  private def getQueryResult[T](url: String, postProcessor:(String) => T)
+      (implicit columns: Array[String] = null, 
+      attrToFilters: Map[String, Array[Filter]] =null) : T={
+    logger.debug("Cloudant query: "+ url)
+    implicit val ( system, existing) = getSystem()
+    import system.dispatcher 
 
-      def getIterator(skip: Int, limit: Int, url: String)(implicit columns: Array[String] =null,attrToFilters: Map[String, Array[Filter]] =null) ={
-        implicit def convertSkip(skip: Int): String  = {
-          val url = config.getLastUrl(skip)
-          if (url == null) skip.toString()
-          else  this.getQueryResult[String](url,{result => config.getLastNum(Json.parse(result)).as[JsString].value})
-        }
-        val newUrl = config.getSubSetUrl(url,skip, limit)
-        this.getQueryResult[Iterator[String]](newUrl,processIterator)
-      }
+    var pipeline: HttpRequest => Future[HttpResponse] = null
+    if (validCredentials!=null){
+      pipeline = ( 
+        addCredentials(validCredentials) 
+        ~> sendReceive
+      )
+    }else{
+      pipeline = sendReceive
+    }
+    val response: Future[HttpResponse] = pipeline(Get(url))
+    val result = Await.result(response, timeout.duration)
+    val data = postProcessor(result.entity.asString)
+    logger.debug(s"got result:$data")
+    if(!existing){
+      logger.info("shutdown newly created ActorSystem")
+      system.shutdown()
+    }
+    data
+  }
 
-      def getTotalRows(url: String) : Int ={
-          val totalUrl = config.getTotalUrl(url)
-          this.getQueryResult[Int](totalUrl,{result => config.getTotalRows(Json.parse(result))})
-      }
-      
-      
-      private def processAll (result: String)( implicit columns: Array[String], attrToFilters: Map[String, Array[Filter]] =null)= {
-          logger.debug(s"processAll columns:$columns, attrToFilters:$attrToFilters")
-          val jsonResult = Json.parse(result)
-          var rows = config.getRows(jsonResult )
-          rows.map(r =>  convert(r))
-      }
-      
-      private def processIterator (result: String)( implicit columns: Array[String], attrToFilters: Map[String, Array[Filter]] =null)= {
-        processAll(result).iterator
-      }
-
-
-      private def convert(rec:JsValue)(implicit columns: Array[String]): String = {
-        if (columns == null) return Json.stringify(Json.toJson(rec))
-        val m = new HashMap[String, JsValue]()
-        for ( x <-columns)
+  def saveAll(rows: Array[String]) {
+    implicit val (system, existing) = getSystem()
+    import system.dispatcher 
+    
+    val useBulk = (config.getBulkPostUrl() != null && config.bulkSize>1)
+    val bulkSize = if (useBulk) config.bulkSize else 1
+    val bulks = rows.grouped(bulkSize).toList
+    
+    val url = if (bulkSize>1) config.getBulkPostUrl() else config.getPostUrl()
+    logger.info(s"Post:$url")
+    
+    if (url == null) return
+    import ContentTypes._
+    implicit val stringMarshaller = Marshaller.of[String](`application/json`) {
+      (value, ct, ctx) => ctx.marshalTo(HttpEntity(ct, value))
+    }
+    val parallelSize = if (config.concurrentSave>0) config.concurrentSave else bulks.size
+    val blocks = bulks.size/parallelSize + (if ( bulks.size % parallelSize != 0) 1 else 0)
+    
+    for (i <- 0 until blocks){
+      val start = parallelSize*i
+      val end = if (parallelSize+start<bulks.size) parallelSize+start else bulks.size
+      logger.info(s"Save from $start to ${end-1} for bulkSize=$bulkSize and paralellSize=$parallelSize at ${i+1}/$blocks")
+      val allFutures =  { 
+        for ( j <- start until end) yield
         {
-            val field = JsonUtil.getField(rec, x).getOrElse(JsNull)
-            m.put(x,  field)
-        }
-        val result = Json.stringify(Json.toJson(m.toMap))
-        logger.debug(s"converted: $result")
-        result
-      }
-      
-      private def getSystem(): (ActorSystem, Boolean) = {
-         val checkSprayConfig = envSystem.settings.config.withFallback(ConfigFactory.parseString("""
-            spray = NOT_FOUND
-         """) )
-         val sprayValue = checkSprayConfig.getValue("spray").unwrapped().toString()
-          if ( !sprayValue.equals("NOT_FOUND")) // The env actorSystem loaded spary config
-          {
-            logger.info("reuse SparkEnv ActorSystem as it contains spray")
-            (envSystem, true)
-          }
-          else{
-            logger.info("create new ActorSystem as the SparkEnv one does not contain spray")
-            val classLoader = this.getClass.getClassLoader
-            val myconfig = ConfigFactory.load(classLoader)// force config from my classloader
-            val nextRamdomNum = new Random().nextInt
-            (ActorSystem("CloudantSpark-"+nextRamdomNum,myconfig,classLoader), false)
-          }
-
-      }
-      private def  getQueryResult[T](url: String, postProcessor:(String) => T)(implicit columns: Array[String] =null, attrToFilters: Map[String, Array[Filter]] =null) : T={
-          logger.info("Cloudant query: "+ url)
-          implicit val ( system, existing) = getSystem()
-          import system.dispatcher 
-
+          val  x: String = if (bulkSize >1) config.getBulkRows(bulks(j)) else bulks(j)(0)
+          logger.debug(s"content:$x")
           var pipeline: HttpRequest => Future[HttpResponse] = null
           if (validCredentials!=null)
           {
             pipeline = ( 
-              addCredentials(validCredentials) 
-              ~> sendReceive
+            addCredentials(validCredentials) 
+            ~> sendReceive
             )
           }else
           {
             pipeline = sendReceive
           }
-          val response: Future[HttpResponse] = pipeline(Get(url))
-          val result = Await.result(response, timeout.duration)
-          val data = postProcessor(result.entity.asString)
-          logger.debug(s"got result:$data")
-          if(!existing)
-          {
-            logger.info("shutdown newly created ActorSystem")
-            system.shutdown()
-          }
-          data
+          val request = Post(url,x)
+          val response: Future[HttpResponse] = pipeline(request)
+            response
+          } 
+        }
+        val f = Future.sequence(allFutures.toList)
+        val result = Await.result(f, timeout.duration)
+        val isSuccessful= result.forall { x => x.status.isSuccess } 
+        if(!existing)
+        {
+          logger.info("shutdown newly created ActorSystem")
+          system.shutdown()
+        }
+        logger.info(s"Save total ${end-start}=${result.length} successful=$isSuccessful")
+        if (!isSuccessful)
+           throw new RuntimeException("Database " + config.getDbname() +
+            " does not exist or is not accessible. Failed to save data!")
       }
-      
-      def saveAll(rows: Array[String]) {
-          implicit val (system, existing) = getSystem()
-          import system.dispatcher 
-          
-          val useBulk = (config.getBulkPostUrl() != null && config.bulkSize>1)
-          val bulkSize = if (useBulk) config.bulkSize else 1
-          val bulks = rows.grouped(bulkSize).toList
-          
-          val url = if (bulkSize>1) config.getBulkPostUrl() else config.getPostUrl()
-          logger.info(s"Post:$url")
-          
-          if (url == null) return
-          import ContentTypes._
-          implicit val stringMarshaller = Marshaller.of[String](`application/json`) {
-            (value, ct, ctx) => ctx.marshalTo(HttpEntity(ct, value))
-          }
-          val parallelSize = if (config.concurrentSave>0) config.concurrentSave else bulks.size
-          val blocks = bulks.size/parallelSize + (if ( bulks.size % parallelSize != 0) 1 else 0)
-          
-          for (i <- 0 until blocks){
-            val start = parallelSize*i
-            val end = if (parallelSize+start<bulks.size) parallelSize+start else bulks.size
-            logger.info(s"Save from $start to ${end-1} for bulkSize=$bulkSize and paralellSize=$parallelSize at ${i+1}/$blocks")
-            val allFutures =  { 
-              for ( j <- start until end) yield
-              {
-                val  x: String = if (bulkSize >1) config.getBulkRows(bulks(j)) else bulks(j)(0)
-                logger.debug(s"content:$x")
-                var pipeline: HttpRequest => Future[HttpResponse] = null
-                if (validCredentials!=null)
-                {
-                  pipeline = ( 
-                  addCredentials(validCredentials) 
-                  ~> sendReceive
-                  )
-                }else
-                {
-                  pipeline = sendReceive
-                }
-                val request = Post(url,x)
-                val response: Future[HttpResponse] = pipeline(request)
-                response
-              } 
-            }
-            val f= Future.sequence(allFutures.toList)
-            val result = Await.result(f, timeout.duration)
-            val isSuccessful= result.forall { x => x.status.isSuccess } 
-            if(!existing)
-            {
-              logger.info("shutdown newly created ActorSystem")
-              system.shutdown()
-            }
-            logger.info(s"Save total ${end-start}=${result.length} successful=$isSuccessful")
-            if (!isSuccessful)
-               result.foreach { x => logger.info(x.message.toString()) }
-          }
-      }
+  }
 }
 
